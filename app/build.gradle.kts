1fca58acaddbffaaf5df6a4393362db8667b41b2
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-09-02 12:07:09 +0900

    [KNK-1111] Chore: 앱 버전 코드 3으로 상향

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,7 +96,7 @@ android {
         applicationId = "app.manyak"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
