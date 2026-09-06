f2fa2227957af0462be1aa1de8d29d545d229ef9
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-09-06 22:13:24 +0900

    [KNK-1211] Feat: 표지 좋아요 수 뱃지 추가
    - 하트 아이콘 자산을 더하고 표지 위 뱃지를 아이콘·문구만 갈리는 한 컴포넌트로 묶음
    - 목록 카드와 상세 히어로가 좋아요 수를 턴 수 왼쪽에 나란히 그림

--- story/src/main/java/app/manyak/story/detail/presentation/component/StoryDetailBody.kt
+++ story/src/main/java/app/manyak/story/detail/presentation/component/StoryDetailBody.kt
@@ -146,6 +146,7 @@ private fun StoryHero(
                     Modifier.clickable(role = Role.Button, onClickLabel = openLabel, onClick = onClick)
                 },
             thumbnailUrl = story.thumbnailUrl,
+            likeCount = story.likeCount,
             turnCount = story.turnCount,
             badgeScale = StoryBadgeScale.Large,
             shape = RectangleShape,
