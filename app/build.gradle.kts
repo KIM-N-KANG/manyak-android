ef2f2c42b124016a4a7af10ffd88c5fc042c2fbb
Dongwoo Kang <alsdn1360@ajou.ac.kr>
2026-09-20 18:32:31 +0900

    [KNK-1351] Chore: 앱 버전 v1.1.1으로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,8 +75,8 @@ android {
         applicationId = "app.manyak"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
